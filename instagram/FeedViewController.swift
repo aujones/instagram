299ad5264cd4cda34c5e557c730a539ee861d293
Audrey Jones <audreyjones@fb.com>
2017-07-02 12:49:19 -0700

    added segue from feed to detail view

--- instagram/FeedViewController.swift
+++ instagram/FeedViewController.swift
@@ -96,14 +96,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        if let indexPath = postTableView.indexPath(for: cell) {
+            let post = posts[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.post = post
+        }
     }
-    */
+    
 
 }
